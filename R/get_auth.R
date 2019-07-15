36f0a4804bce79aa33a75d60b62c2c38c8535014
jburkhardt <johannes.burkhardt@gmail.com>
2019-07-15 10:46:06 +0200

    working authentication process

---
+++ R/get_auth.R
@@ -0,0 +1,58 @@
+#' @title Authentication of R app
+#' @description get_auth authenticates the R app at the Google authentication server using OAUTH2 and receives the client token.
+#' Usually you need not to run get_auth() explicitly since the whole authentication process is managed by \code{\link{do_auth}}.
+#' @importFrom utils browseURL
+#' @importFrom RCurl getURL
+#' @return Client token from Google authentication server.
+get_auth = function() {
+  # This function asks for the credentials (Client Id, Client Secret) from the Api project,
+  # the developer token from the Adwords MCC and retrieves the client token from the Google authentication server.
+  # Moreover credentials optionally can be saved to the current working directory and .gitignore is generated for credentials.
+  #
+  # Inputs:
+  #   Client Id from Google Api project.
+  #   Client secret from Google Api project.
+  #   Developer token from Adwords MCC.
+  #
+  # Returns:
+  #   Client token from Google authentication server.
+  if(!exists('credentials')){
+    cat('Authentication process needs your Client ID from the Adwords API project for native apps.')
+    c.id <- readline(as.character(cat("\n\nPaste the Client ID here",
+                                      ":=>")))
+    if(c.id == ""){
+      return(print('You have to provide a Client ID from the Adwords API Project for native apps.'))
+    }
+    else {
+      credentials <- data.frame(c.id)
+      cat('Authentication process needs your Client secret from the Adwords API project.')
+      credentials$c.secret <- readline(as.character(cat("\n\nPaste the Client secret here",
+                                                        ":=>")))
+      cat('Authentication process needs your Developer Token from the Adwords MCC.')
+      credentials$auth.developerToken <- readline(as.character(cat("\n\nPaste the Developer Token here",
+                                                                   ":=>")))
+    }
+  }
+
+  if(exists('credentials')){
+    url <- paste('https://accounts.google.com/o/oauth2/auth?',
+                 'client_id=', credentials$c.id, '&',
+                 'response_type=code&',
+                 'scope=https://www.googleapis.com/auth/adwords&',
+                 'redirect_uri=urn:ietf:wg:oauth:2.0:oob&',
+                 'access_type=offline&',
+                 'approval_prompt=force', sep='', collapse='')
+    #cert <- system.file("CurlSSL", "ca-bundle.crt", package = "RCurl")#SSL Certificate Fix for Windows
+    RCurl::getURL(url,
+                  #cainfo=cert, # Explicitly setting certificate verification for an error in OS X
+                  ssl.verifypeer = TRUE)
+    browseURL(url)
+    # Manual next-step: input code-parameter to c.token variable and run load_token()
+    cat('Authentication process needs your Client token in order to receive the access token from the API. Copy the Client token from your webbrowser and paste it here.')
+    credentials$c.token <- readline(as.character(cat("\n\nPaste the client token here",
+                                                     ":=>")))
+  }
+  # return one environment that contains all the values...
+  # call it by credential_env <- get_auth()
+  credentials
+}
---
+++ R/refresh_token.R
@@ -0,0 +1,27 @@
+#' @title Refresh Access Token
+#' @description refresh_token returns a new valid access token. The access token deprecates after one hour and has to updated with the refresh token.
+#' Usually you need not to run refresh_token() explicitly since the whole authentication process is managed by \code{\link{do_auth}}.
+#' @param google_auth list of credentials and access token
+#' @return New access token with corresponding time stamp.
+refresh_token = function(google_auth) {
+  # This function refreshes the access token. The access token deprecates after one hour and has to updated with the refresh token.
+  #
+  # Args:
+  #   access.token$refreh_token and credentials as input
+  # Returns:
+  #   New access.token with corresponding time stamp
+  raw_data = RCurl::postForm('https://accounts.google.com/o/oauth2/token',
+                             refresh_token=google_auth$access$refresh_token,
+                             client_id=google_auth$credentials$c.id,
+                             client_secret=google_auth$credentials$c.secret,
+                             grant_type="refresh_token",
+                             style="POST",
+                             .opts = list(ssl.verifypeer = FALSE)) #Fix SSL Certificate problem on Windows
+  if(is.raw(raw_data)) {
+    rt = rjson::fromJSON(rawToChar(raw_data))
+  } else {
+    rt = rjson::fromJSON(raw_data)
+  }
+  access <- rt
+  access
+}
