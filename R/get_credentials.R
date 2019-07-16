17118c3cfad2b70d17750d78d4d6a41128b8e771
jburkhardt <johannes.burkhardt@gmail.com>
2019-07-16 10:20:59 +0200

    fix format of credentials.id from factor to character

--- R/get_credentials.R
+++ R/get_credentials.R
@@ -1,46 +1,46 @@
 #' @title Authentication of R app
-#' @description get_auth authenticates the R app at the
+#' @description get_credentials authenticates the R app at the
 #' Google authentication server using OAUTH2 and receives the client token.
-#' Usually you need not to run get_auth() explicitly since the whole authentication process is managed by \code{\link{do_auth}}.
+#' Usually you need not to run get_credentials() explicitly since the whole authentication process is managed by \code{\link{authenticate}}.
 #' @importFrom utils browseURL
 #' @importFrom RCurl getURL
 #' @return Client token from Google authentication server.
 get_credentials <- function() {
   # This function asks for the credentials
   # (Client Id, Client Secret) from the Api project,
-  # the developer token from the Adwords MCC and
+  # the developer token from the Google Ads MCC and
   # retrieves the client token from the Google authentication server.
   # Moreover credentials optionally can be saved to the current working directory and .gitignore is generated for credentials.
   #
   # Inputs:
   #   Client Id from Google Api project.
   #   Client secret from Google Api project.
-  #   Developer token from Adwords MCC.
+  #   Developer token from Google Ads MCC.
   #
   # Returns:
   #   Client token from Google authentication server.
   if (!exists("credentials")) {
     cat("Authentication process needs your Client ID
-        from the Adwords API project for native apps.")
+        from the Google Ads API project for native apps.")
     c.id <- readline(as.character(cat(
       "\n\nPaste the Client ID here",
       ":=>"
     )))
     if (c.id == "") {
       return(print("You have to provide a Client ID
-                   from the Adwords API Project for native apps."))
+                   from the Google Ads API Project for native apps."))
     }
     else {
-      credentials <- data.frame(c.id)
+      credentials <- data.frame(c.id, stringsAsFactors = F)
       cat("Authentication process needs your Client
-          secret from the Adwords API project.")
+          secret from the Google Ads API project.")
       credentials$c.secret <-
         readline(as.character(cat(
           "\n\nPaste the Client secret here",
           ":=>"
         )))
       cat("Authentication process needs your
-          Developer Token from the Adwords MCC.")
+          Developer Token from the Google Ads MCC.")
       credentials$auth.developerToken <-
         readline(as.character(cat(
           "\n\nPaste the Developer Token here",
