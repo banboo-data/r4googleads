703736fbfe5d3b645724270f4d06c8ea881e59ac
jburkhardt <johannes.burkhardt@gmail.com>
2019-07-17 16:16:50 +0200

    refresh_token.R - replace RCurl by curl

--- R/refresh_token.R
+++ R/refresh_token.R
@@ -2,6 +2,8 @@
 #' @description refresh_token returns a new valid access token. The access token deprecates after one hour and has to updated with the refresh token.
 #' Usually you need not to run refresh_token() explicitly since the whole authentication process is managed by \code{\link{authenticate}}.
 #' @param google_auth list of credentials and access token
+#' @importFrom curl new_handle handle_setform curl_fetch_memory
+#' @importFrom jsonlite fromJSON prettify
 #' @return New access token with corresponding time stamp.
 refresh_token <- function(google_auth) {
   # This function refreshes the access token. The access token deprecates after one hour and has to updated with the refresh token.
@@ -10,19 +12,14 @@ refresh_token <- function(google_auth) {
   #   access.token$refreh_token and credentials as input
   # Returns:
   #   New access.token with corresponding time stamp
-  raw_data <- RCurl::postForm("https://accounts.google.com/o/oauth2/token",
-    refresh_token = google_auth$access$refresh_token,
-    client_id = google_auth$credentials$c.id,
-    client_secret = google_auth$credentials$c.secret,
-    grant_type = "refresh_token",
-    style = "POST",
-    .opts = list(ssl.verifypeer = FALSE)
-  ) # Fix SSL Certificate problem on Windows
-  if (is.raw(raw_data)) {
-    rt <- rjson::fromJSON(rawToChar(raw_data))
-  } else {
-    rt <- rjson::fromJSON(raw_data)
-  }
-  access <- rt
-  access
+  h <- curl::new_handle()
+  curl::handle_setform(h,
+                       refresh_token = google_auth$access$refresh_token,
+                       client_id = google_auth$credentials$c.id,
+                       client_secret = google_auth$credentials$c.secret,
+                       grant_type = "refresh_token",
+                       style = "POST")
+  req <- curl::curl_fetch_memory("https://accounts.google.com/o/oauth2/token", handle = h)
+  a <- jsonlite::fromJSON(jsonlite::prettify(rawToChar(req$content)))
+  a
 }
