c2afde18e2bfe52a724bbc614f7032f5a56f4e78
jburkhardt <johannes.burkhardt@gmail.com>
2019-07-17 14:34:46 +0200

    get_credentials.R - uses sprintf instead of paste

--- R/get_credentials.R
+++ R/get_credentials.R
@@ -50,17 +50,9 @@ get_credentials <- function() {
   }
 
   if (exists("credentials")) {
-    url <- paste("https://accounts.google.com/o/oauth2/auth?",
-      "client_id=", credentials$c.id, "&",
-      "response_type=code&",
-      "scope=https://www.googleapis.com/auth/adwords&",
-      "redirect_uri=urn:ietf:wg:oauth:2.0:oob&",
-      "access_type=offline&",
-      "approval_prompt=force",
-      sep = "", collapse = ""
-    )
-    # cert <- system.file("CurlSSL", "ca-bundle.crt",
-    # package = "RCurl")#SSL Certificate Fix for Windows
+    url <- sprintf("https://accounts.google.com/o/oauth2/auth?client_id=%s&response_type=code&scope=https://www.googleapis.com/auth/adwords&redirect_uri=urn:ietf:wg:oauth:2.0:oob&access_type=offline&approval_prompt=force",
+                   credentials$c.id)
+    # cert <- system.file("CurlSSL", "ca-bundle.crt", package = "RCurl")#SSL Certificate Fix for Windows
     RCurl::getURL(url,
       # cainfo=cert, # Explicitly setting
       # certificate verification for an error in OS X
