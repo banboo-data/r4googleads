32d21e29b83c64b9b0bd7d546fa34f4835825897
jburkhardt <johannes.burkhardt@gmail.com>
2019-07-19 12:22:29 +0200

    protoype reporting function that queries data from the API

---
+++ R/query_data.R
@@ -0,0 +1,66 @@
+#' @title Query Google Ads Data
+#' @description Queries data from Google Ads API.
+#' @param account_id Google Ads Account ID
+#' @param mcc_id Google Ads Client Center MCC Id
+#' @param google_auth auth object
+#' @param query Google Ads query
+#' @importFrom curl new_handle handle_setheaders handle_setform curl_fetch_memory
+#' @importFrom jsonlite fromJSON
+#' @return Dataframe
+#' @export
+query_data <- function(account_id,
+                       mcc_id,
+                       google_auth,
+                       query = "SELECT campaign.name, campaign.status, segments.device,
+                                metrics.impressions, metrics.clicks, metrics.ctr,
+                                metrics.average_cpc, metrics.cost_micros
+                                FROM campaign
+                                WHERE segments.date DURING LAST_30_DAYS"){
+
+  account_id <- gsub("-", "", account_id)
+  mcc_id <- gsub("-", "", mcc_id)
+  access <- google_auth$access
+  credlist <- google_auth$credentials
+  google.auth <- paste(access$token_type, access$access_token)
+
+  url <- sprintf("https://googleads.googleapis.com/v2/customers/%s/googleAds:search", account_id)
+
+  statement <- sprintf('{"query" : "%s"}', query)
+
+  header <- c("Host" = "googleads.googleapis.com",
+              "User-Agent" = "curl",
+              "Content-Type" = "application/json",
+              "Accept" = "application/json",
+              "Authorization" = google.auth,
+              "developer-token" = credlist$auth.developerToken,
+              "login-customer-id" = mcc_id)
+
+  data <- RCurl::getURL(url,
+                        httpheader = header,
+                        postfields=statement,
+                        verbose = TRUE)
+
+  data <- fromJSON(data)
+
+  data$results
+
+  # h <- new_handle()
+  # handle_setheaders(h,
+  #                   "Host" = "googleads.googleapis.com",
+  #                   "User-Agent" = "curl",
+  #                   "Content-Type" = "application/json",
+  #                   "Accept" = "application/json",
+  #                   "Authorization" = google.auth,
+  #                   "developer-token" = credlist$auth.developerToken,
+  #                   "login-customer-id" = mcc_id
+  # )
+  #
+  # handle_setform(h,
+  #                postfields=statement)
+  #
+  # req <- curl_fetch_memory(url,
+  #                          handle = h)
+  #
+  # a <- fromJSON(rawToChar(req$content))
+
+}
