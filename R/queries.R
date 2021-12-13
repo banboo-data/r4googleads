abbc27c174de831cad373982f5fe575413e7d6fb
jburkhardt <johannes.burkhardt@gmail.com>
2021-12-13 18:22:54 +0100

    migrates package towards latest Google Ads API version v9, closes #17

--- R/queries.R
+++ R/queries.R
@@ -1,7 +1,9 @@
 #' Query Strings for Query Based Services
 #'
-#' A built-in collection of queries, mostly fot the googleAdsSearch service: default_search(), ...
-#' Usually these functions are not called stand alone. Often they are used when a constructor of a query based service is called.
+#' A built-in collection of queries, mostly for the googleAdsSearch service: default_search(), ...
+#' Usually these functions are not called stand alone. Often they are used when a constructor of
+#' a query based service is called.
+#' A Google Ads Query Builder can be found here: \url{https://developers.google.com/google-ads/api/fields/v9/overview_query_builder}
 #'
 #' @return character query statement
 #' @rdname queries
@@ -12,5 +14,7 @@ default_search <- function() {
            metrics.clicks, metrics.ctr,
            metrics.average_cpc, metrics.cost_micros
            FROM campaign
-           WHERE segments.date DURING LAST_30_DAYS")
+           WHERE segments.date DURING LAST_30_DAYS
+           AND metrics.impressions > 0
+           PARAMETERS include_drafts=true")
 }
